0a99fa05fa475afa0d3c1d98efc0187228691ed0
YoungLee <youngleemails@gmail.com>
2017-11-21 16:10:59 +0800

    initial

--- src/to-string.js
+++ src/to-string.js
@@ -18,7 +18,16 @@ module.exports.pitch = function(remainingRequest) {
 
         if (typeof result === "string") {
             module.exports = result;
-        } else {
+        } else if (typeof result === "object") {
+            for (var prop in result) {
+                if (result.hasOwnProperty(prop)) {
+                    if (result[prop] instanceof Function) {
+                        result[prop] = result[prop].toString();
+                    }
+                }
+            }
+            module.exports = result;
+        } else if (typeof result === "function") {
             module.exports = result.toString();
         }
     `;
